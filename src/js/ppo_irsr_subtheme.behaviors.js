fcdfce25f8be1dbff57c689909fa6468910e61da
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-11-09 19:49:01 +0000

    Add search button to nav

--- src/js/ppo_irsr_subtheme.behaviors.js
+++ src/js/ppo_irsr_subtheme.behaviors.js
@@ -2,6 +2,8 @@
   'use strict';
   Drupal.behaviors.ppoIrsrSubtheme = {
     attach: function (context) {
+      const menuSearchButton = $('<button>', { class: 'menu-search-button su-site-search__submit su-sr-only-text', 'name': 'search' });
+      $('.su-multi-menu', context).append(menuSearchButton);
 
       // Replace <p> tag for quote-text banner with <blockquote>
       $(once('quote-text', '.irsr-wrapper--banner-quote-text', context))
