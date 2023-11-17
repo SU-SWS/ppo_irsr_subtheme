a20fad4c3f1fa11d0212f2343fa0e5c566d8ed2f
jdwjdwjdw <jacobdeanwilson@gmail.com>
2023-11-17 19:47:05 +0000

    Update search link to use font awesome

--- src/js/ppo_irsr_subtheme.behaviors.js
+++ src/js/ppo_irsr_subtheme.behaviors.js
@@ -2,8 +2,13 @@
   'use strict';
   Drupal.behaviors.ppoIrsrSubtheme = {
     attach: function (context) {
-      const menuSearchButton = $('<a>', { class: 'menu-search-button su-site-search__submit', 'href': '/search', 'aria-label': 'Go to search page' });
-      $('.su-multi-menu', context).append(menuSearchButton);
+      // const searchSpan = $('<span />', { 'aria-hidden': 'true' }).prependTo(menuSearchButton);
+      // const searchSpan = $('<span />');
+      // const searchIcon = $('<i />', { class: 'fas fa-search'}).prependTo(searchSpan);
+      const searchIcon = $('<i />', { class: 'fas fa-search'});
+      const menuSearchLink = $('<a>', { class: 'menu-search-link', 'href': '/search', 'aria-label': 'Go to search page' }).prepend(searchIcon);
+      // const menuSearchButton = $('<a>', { class: 'menu-search-button', 'href': '/search', 'aria-label': 'Go to search page' }).prepend(searchSpan);
+      $('.su-multi-menu', context).append(menuSearchLink);
 
       // Replace <p> tag for quote-text banner with <blockquote>
       $(once('quote-text', '.irsr-wrapper--banner-quote-text', context))
