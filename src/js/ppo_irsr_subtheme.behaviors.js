3c677f14cf9b9171d588bd703017dfd913b6cbb0
Jacob Wilson <49299083+jdwjdwjdw@users.noreply.github.com>
2023-11-17 17:06:20 -0800

    IRSR24-28 | @jdwjdwjdw | Add search button to nav (#10)
    * Add search button to nav
    
    * Update search styles and structure
    
    * Add aria-label, updated search icon color on hover, focus, and active
    
    * Remove unnecessary class
    
    * Update version number
    
    * Decrease search icon size
    
    * Update search link to use font awesome
    
    * Update search icon styles
    
    * Update search link class
    
    Co-authored-by: Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>
    
    * Update dist
    
    ---------
    
    Co-authored-by: Rebecca Hong <34019925+rebeccahongsf@users.noreply.github.com>

--- src/js/ppo_irsr_subtheme.behaviors.js
+++ src/js/ppo_irsr_subtheme.behaviors.js
@@ -2,6 +2,9 @@
   'use strict';
   Drupal.behaviors.ppoIrsrSubtheme = {
     attach: function (context) {
+      const searchIcon = $('<i />', { class: 'fas fa-search'});
+      const menuSearchLink = $('<a>', { class: 'menu-search-link', 'href': '/search', 'aria-label': 'Go to search page' }).prepend(searchIcon);
+      $('.su-multi-menu', context).append(menuSearchLink);
 
       // Replace <p> tag for quote-text banner with <blockquote>
       $(once('quote-text', '.irsr-wrapper--banner-quote-text', context))
